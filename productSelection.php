5a01fa3903af3839760b5aebd29715ebf1d6133f
clement <c.gambier@gustosolutions.fr>
2023-06-12 09:37:26 +0200

    feat(WB-1): Basic PHP API

---
+++ productSelection.php
@@ -0,0 +1,37 @@
+<?php
+//config
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+$servername = "localhost"; 
+$username = "clement"; 
+$password = "avocadoForever";
+$database = "avocado";
+
+//connect to db
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Erreur de connexion à la base de données : " . $conn->connect_error);
+}
+
+// get all the products from the database
+$sql = "SELECT * FROM products LIMIT 3";
+$result = $conn->query($sql);
+
+// check errors
+if (!$result) {
+    die("Erreur d'exécution de la requête : " . $conn->error);
+}
+$data = array();
+
+while ($row = $result->fetch_assoc()) {
+    $data[] = $row;
+}
+
+//close connection
+$conn->close();
+
+// Send data
+echo json_encode($data);
+?>
